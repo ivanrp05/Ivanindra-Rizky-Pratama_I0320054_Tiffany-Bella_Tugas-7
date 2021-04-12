64eec65527b2046a5b4373d29b03d77db80bc5f3
I van <80109751+ivanrp05@users.noreply.github.com>
2021-04-12 22:36:12 +0700

    Add files via upload
    Selamat malam, saya Ivanindra Rizky P (I0320054) izin mengumpulkan penugasan ke 7 praktikum programa komputer. Terima kasih

---
+++ I0320054_Soal1_Tugas7.py
@@ -0,0 +1,52 @@
+#Mulai
+print("Soal 1 Tugas 7 Praktikum Programa Komputer ")
+print("")
+print("===========================")
+print("Nama : Ivanindra Rizky P")
+print("NIM : I0320054")
+print("")
+print("===========================")
+print("")
+print("Ditanya :")
+print("Buatlah 1 program menggunakan fungsi string (Min 3 jenisfungsi string)")
+print("===========================")
+print("")
+print("Jawab :")
+print("")
+print("===========================")
+print("")
+x = str(input("Masukkan kata/kalimat yang anda inginkan : "))
+print("")
+print("Kata/kalimat yang anda masukkan akan kami Capitalize, menjadi : ")
+print("")
+a = x.capitalize()
+print(a)
+print("")
+print("Kata/kalimat yang anda masukkan akan kami Center, menjadi : ")
+str = x
+b = str.center(50,'=')
+print(b)
+print("")
+c = input("Masukkan huruf yang anda ingin hitung jumlahnya pada kalimat tersebut : ")
+str = x
+d = str.count(c)
+print("Jumlah huruf", c, "adalah : ", d)
+print("")
+e = input("Masukkan kata yang ingin anda replace : ")
+print("")
+f = input("Masukkan kata pengganti : ")
+print("")
+print("kata yang ingin anda replace adalah : ", e,"menjadi kata : ", f)
+print("")
+str = x
+print("Hasil dari kata baru adalah : ", str.replace(e,f))
+print("")
+print("Ini adalah akhir dari pemrograman")
+print("")
+input("Klik enter untuk kembali.....")
+print("Note : mohon maaf apabila terdapat kesalahan")
+#Selesai
+
+
+
+
